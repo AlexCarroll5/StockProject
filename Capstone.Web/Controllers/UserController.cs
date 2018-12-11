53eedb45ddb4b9632f4d671638cfc99c71183784
Lucas Frazier <lucasfrazier1@gmail.com>
2018-12-11 14:49:03 -0500

    Blah

--- Capstone.Web/Controllers/UserController.cs
+++ Capstone.Web/Controllers/UserController.cs
@@ -19,13 +19,122 @@ namespace Capstone
         {
             return View();
         }
-        public ActionResult Register()
-        {
 
-        }
+        [HttpGet]
         public ActionResult Login()
         {
+            if (base.IsAuthenticated)
+            {
+                LogUserOut();
+            }
 
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model)
+        {
+            ActionResult result = null;
+
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    throw new Exception();
+                }
+
+                UserItem user = null;
+                try
+                {
+                    user = _db.GetUserItem(model.Username);
+                }
+                catch (Exception)
+                {
+                    ModelState.AddModelError("invalid-user", "Either the username or the password is invalid.");
+                    throw;
+                }
+
+                PasswordHelper passHelper = new PasswordHelper(model.Password, user.Salt);
+                if (!passHelper.Verify(user.Hash))
+                {
+                    ModelState.AddModelError("invalid-user", "Either the username or the password is invalid.");
+                    throw new Exception();
+                }
+
+                // Happy Path
+                base.LogUserIn(user);
+
+                result = RedirectToAction("Index", "Vending");
+            }
+            catch (Exception)
+            {
+                result = View("Login");
+            }
+
+            return result;
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            if (base.IsAuthenticated)
+            {
+                LogUserOut();
+            }
+
+            return View("Register");
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            ActionResult result = null;
+
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    throw new Exception();
+                }
+
+                UserItem user = null;
+                try
+                {
+                    user = _db.GetUserItem(model.Username);
+                }
+                catch (Exception)
+                {
+                }
+
+                if (user != null)
+                {
+                    ModelState.AddModelError("invalid-user", "The username is already taken.");
+                    throw new Exception();
+                }
+
+                PasswordHelper passHelper = new PasswordHelper(model.Password);
+                UserItem newUser = new UserItem()
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    Email = model.Email,
+                    Username = model.Username,
+                    Salt = passHelper.Salt,
+                    Hash = passHelper.Hash,
+                    RoleId = (int)RoleManager.eRole.Customer
+                };
+
+                _db.AddUserItem(newUser);
+                base.LogUserIn(newUser);
+
+                result = RedirectToAction("Index", "Vending");
+            }
+            catch (Exception)
+            {
+                result = View("Register");
+            }
+
+            return result;
         }
     }
 }
