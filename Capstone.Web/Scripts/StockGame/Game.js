fedd11fbea9c202e000066283639cafcd67cf0f7
Michael McClean <mdmcclean10@gmail.com>
2018-12-15 16:29:52 -0500

    pushin for lucas

--- Capstone.Web/Scripts/StockGame/Game.js
+++ Capstone.Web/Scripts/StockGame/Game.js
@@ -60,10 +60,10 @@
         }).done(function (data) {
             for (let i = 0; i < data._userStocks.length; i++) {
                 if (data._userStocks[i].Shares > 0) {
-                    $("#sharesAndCBOf" + data._userStocks[i].UserStock.StockID).text(data._userStocks[i].Shares + "($" + data._userStocks[i].PurchasePrice.toFixed(2) + ")");
+                    $("#sharesOf" + data._userStocks[i].UserStock.StockID).text(data._userStocks[i].Shares + "($" + data._userStocks[i].PurchasePrice.toFixed(2) + ")");
                 }
                 else {
-                    $("#sharesAndCBOf" + data._userStocks[i].UserStock.StockID).text("");
+                    $("#sharesOf" + data._userStocks[i].UserStock.StockID).text("");
                 }
             }
         });
@@ -75,81 +75,7 @@
             url: ajaxURL + "api/ListOfAvailableStocks",
             type: "GET",
             dataType: "json"
-
-
-            //< tr >
-            //<th scope="row">5($9.42)</th>
-            //<td>SNAP</td>
-            //<td>Snapchat</td>
-            //<td>$10.53</td>
-            //<td><button type="button" class="btn btn-success btn-sm">Buy</button></td>
-            //<td><button type="button" class="btn btn-danger btn-sm">Sell</button></td>
-            //<td>xxx</td>
-            //    </tr>
-
         }).done(function (data) {
-            //for (let i = 0; i < data._stocks.length; i++) {
-                //let stockBlock = $("<div>").addClass("row");
-                //let stockSymbol = $("<div>").text(data._stocks[i].Symbol).addClass("col");
-                //let companyName = $("<div>").text(data._stocks[i].CompanyName).addClass("col");
-                //let price = $("<div>").text(data._stocks[i].CurrentPrice).addClass("col");
-                //var sharesToBuySell = document.createElement('input');
-                //sharesToBuySell.type = "number"; 
-                //sharesToBuySell.id = "stockID" + data._stocks[i].StockID;
-                //var buyButton = document.createElement('button');
-                //buyButton.id = "buyStockId" + data._stocks[i].StockID;
-                //buyButton.innerText = "Buy " + data._stocks[i].Symbol;
-                //var sellButton = document.createElement('button');
-                //sellButton.id = "sellStockId" + data._stocks[i].StockID;
-                //sellButton.innerText = "Sell " + data._stocks[i].Symbol;
-                //stockBlock.append(stockSymbol);
-                //stockBlock.append(companyName);
-                //stockBlock.append(price);
-                //stockBlock.append(sharesToBuySell).addClass('col');
-                //stockBlock.append(buyButton).addClass('col');
-                //stockBlock.append(sellButton).addClass('col');
-
-                //$("#stocks").append(stockBlock);
-
-                //let stockTableRow = $("<tr>");
-                //let stockH = $('<th scope="row">');
-                //let stockSymbol = $("<td>").text(data._stocks[i].Symbol);
-                //let companyName = $("<td>").text(data._stocks[i].CompanyName);
-                //let price = $("<td>").text(data._stocks[i].CurrentPrice);
-                //var sharesToBuySell = document.createElement('input');
-                //sharesToBuySell.type = "number";
-                //sharesToBuySell.id = "stockID" + data._stocks[i].StockID;
-                //let sharesInput = $("<td>").attr("width", 15);
-                //sharesInput.append(sharesToBuySell);
-                //var buyButton = document.createElement('button');
-                //buyButton.id = "buyStockId" + data._stocks[i].StockID;
-                //buyButton.innerText = "Buy";
-                //buyButton.onclick = function () {
-                //    BuyStock(data._stocks[i].StockID, 1);
-                //}
-                //$(buyButton).addClass("btn").addClass("btn-success").addClass("btn-sm");
-                //let bButtonCol = $("<td>")
-                //bButtonCol.append(buyButton);
-                //var sellButton = document.createElement('button');
-                //sellButton.id = "sellStockId" + data._stocks[i].StockID;
-                //sellButton.innerText = "Sell";
-                //$(sellButton).addClass("btn").addClass("btn-danger").addClass("btn-sm");
-                //let sButtonCol = $("<td>");
-                //sButtonCol.append(sellButton);
-
-                //stockTableRow.append(stockH);
-                //stockTableRow.append(stockSymbol);
-                //stockTableRow.append(companyName);
-                //stockTableRow.append(price);
-                //stockTableRow.append(bButtonCol);
-                //stockTableRow.append(sButtonCol);
-                //stockTableRow.append(sharesInput);
-
-                //$("#stockTable").append(stockTableRow);
-
-
-
-            //}
             GetAvailableStocks(data);
         });
 
@@ -224,9 +150,8 @@
         for (let i = 0; i < data._stocks.length; i++) {
 
             let stockTableRow = $("<tr>");
-            let stockH = $('<th scope="row">').attr("id","sharesAndCBOf" + data._stocks[i].StockID);
-            let stockSymbol = $("<td>").text(data._stocks[i].Symbol);
-            let companyName = $("<td>").text(data._stocks[i].CompanyName);
+            let stockShares = $('<td>').attr("id","sharesOf" + data._stocks[i].StockID);
+            let stockSymbol = $("<td>").text(data._stocks[i].Symbol).addClass("stockSymbol");
             let price = $("<td>").text("$" + data._stocks[i].CurrentPrice.toFixed(2)).attr("id", "priceOf" + data._stocks[i].StockID);
             var sharesToBuySell = document.createElement('input');
             sharesToBuySell.type = "text";
@@ -254,10 +179,14 @@
             let sButtonCol = $("<td>");
             sButtonCol.append(sellButton);
 
-            stockTableRow.append(stockH);
+            let avgCol = $("<td>").attr("id", "costBasisOf" + data._stocks[i].StockID);
+            let gainLoss = $("<td>").attr("id", "gainLossOf" + data._stocks[i].StockID);
+
             stockTableRow.append(stockSymbol);
-            stockTableRow.append(companyName);
             stockTableRow.append(price);
+            stockTableRow.append(stockShares);
+            stockTableRow.append(avgCol);
+            stockTableRow.append(gainLoss);
             stockTableRow.append(bButtonCol);
             stockTableRow.append(sButtonCol);
             stockTableRow.append(sharesInput);
