7284af615246ec6e22af01467f868795ced6bfd0
Michael McClean <mdmcclean10@gmail.com>
2018-12-18 10:05:33 -0500

    available shares

--- Capstone.Web/Global.asax.cs
+++ Capstone.Web/Global.asax.cs
@@ -30,8 +30,8 @@ namespace Capstone.Web
             //These commented out lines are for a mock if we ever decide to make one #springbreak
             //kernel.Bind<IStockGameDAL>().To<MockStockGameDal>();
             //string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-            //string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
-            string connectionString = ConfigurationManager.ConnectionStrings["LocalConnection"].ConnectionString;
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            //string connectionString = ConfigurationManager.ConnectionStrings["LocalConnection"].ConnectionString;
             kernel.Bind<IStockGameDAL>().To<StockGameDAL>().WithConstructorArgument("connectionString", connectionString);
 
 
