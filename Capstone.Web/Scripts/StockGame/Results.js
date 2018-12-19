cde806ae00c12a4b335909bca5a1167009d33036
Lucas Frazier <lucasfrazier1@gmail.com>
2018-12-19 16:57:25 -0500

    Stufff

---
+++ Capstone.Web/Scripts/StockGame/Results.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+    let ajaxURL = "http://localhost:55601/";
+    //let ajaxURL = "http://192.168.51.117/SMGame/";
+    $(".new-game-button").click(NewGame);
+
+    function NewGame() {
+        $.ajax({
+            url: ajaxURL + "api/CheckSetting",
+            type: "GET",
+            dataType: "json"
+        }).done(function (data) {
+            if (data.SettingValue == 0) {
+                SwitchSetting();
+                location.replace(ajaxURL + "StockGame/Settings");
+            }
+            else {
+                location.replace(ajaxURL + "StockGame/Game");
+            }
+        });    
+    }
+
+    function SwitchSetting() {
+
+        $.ajax({
+            url: ajaxURL + "api/SwitchSettings",
+            type: "POST",
+            data: {
+                switched: false,
+            },
+            dataType: "json"
+        }).done(function (data) { });
+    }
+})
